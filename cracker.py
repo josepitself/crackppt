f0e874fc467647c6da3018782baf8cfe6c35ea24
josepitself <59957552+josepitself@users.noreply.github.com>
2024-04-01 16:28:33 +0200

    ignorar fitxers de "data/" completat generador (tot en RAM i output final a fitxer)

--- cracker.py
+++ cracker.py
@@ -1,3 +1,4 @@
+from itertools import product
 
 def toogle_case(s):
     """
@@ -19,3 +20,39 @@ def all_case_combinations(termsvector):
     for i in range(0, len(termsvector)):
         result.append(toogle_case(termsvector[i]))
     return result
+
+def generate_word_combinations(matrix):
+    """
+    Generate all possible combinations of words from different vectors in the given matrix,
+    where each word appears exactly once in each combination.
+    """
+    # Generate the cartesian product of lists in the matrix
+    combinations = list(product(*matrix))
+    return [list(comb) for comb in combinations]
+
+def generate_strings_with_separators(matrix, separators):
+    """
+    Generate a list of strings where each string is formed by concatenating the words
+    in each row of the matrix with all possible combinations of the given separators.
+    Each pair of words can only be separated by one separator, but different separators
+    can be used within the same string.
+    """
+    def all_combinations(words, sep):
+        """Helper function to recursively find all combinations for a given list of words."""
+        if len(words) == 1:
+            return words
+        # Recur for the rest of the words
+        rest_combinations = all_combinations(words[1:], sep)
+        result = []
+        # For each combination and separator, prepend the first word and separator to the combination
+        for combination in rest_combinations:
+            for s in sep:
+                result.append(words[0] + s + combination)
+        return result
+
+    result_list = []
+    # Generate combinations for each row in the matrix
+    for row in matrix:
+        result_list.extend(all_combinations(row, separators))
+    
+    return result_list
--- genpwd2.py
+++ genpwd2.py
@@ -1,5 +1,27 @@
 import cracker
 
 terms = ['darth', 'vader', 'vs', 'luke', 'skywalker']
+separators = [ '', ' ' ]
+fitxer = './passwords.txt'
 
+print("Step1...")
 tcases = cracker.all_case_combinations(terms)
+all_words = cracker.generate_word_combinations(tcases)
+all_combinations = cracker.generate_strings_with_separators(all_words, separators)
+
+print("writing file...")
+file = open(fitxer, 'w')
+for item in all_combinations:
+    file.write(f'{item}\n')
+
+print("Step2...")
+terms = [ 'luke', 'skywalker', 'vs', 'darth', 'vader' ]
+separators = [ '', ' ' ]
+tcases = cracker.all_case_combinations(terms)
+all_words = cracker.generate_word_combinations(tcases)
+all_combinations = cracker.generate_strings_with_separators(all_words, separators)
+print("writing file...")
+for item in all_combinations:
+    file.write(f'{item}\n')
+
+print("Done!")

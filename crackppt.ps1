0a69e5e4cd0da7b255b1c5468803edc8fd90fed0
josepitself <59957552+josepitself@users.noreply.github.com>
2024-04-18 14:43:11 +0200

    corregit bug que no guardava el punt de "resume"

--- crackppt.ps1
+++ crackppt.ps1
@@ -34,53 +34,61 @@ if (Test-Path -Path $ResumeFile) {
 
 # Intenta obrir el fitxer de PowerPoint amb cada contrasenya de la llista
 $continue = $true
-[console]::TreatControlCAsInput = $true
-while ( ($line = $reader.ReadLine()) -and $continue) {
-    $Password = $line
-    if ($pwds % $VerboseBatch -eq 0) {
-        Write-Host "`rTrying key #$($pwds): '$Password'                                    " -NoNewline
+try {
+    [console]::TreatControlCAsInput = $true
+    while ( ($line = $reader.ReadLine()) -and $continue) {
+        $Password = $line
+        if ($pwds % $VerboseBatch -eq 0) {
+            Write-Host "`rTrying key #$($pwds): '$Password'                                    " -NoNewline
+        }
+        # Intenta obrir el fitxer de PowerPoint 
+        try {
+            $Presentation = $PowerPoint.ProtectedViewWindows.Open($PowerPointFile, $Password)
+            $Presentation.Close()
+            $PowerPoint.Quit()
+            $trencat = 1
+            $continue = $false
+            break
+        }
+        catch {
+        }
+
+        if ([console]::KeyAvailable)
+        {
+            $key = [system.console]::readkey($true)
+            if (($key.modifiers -band [consolemodifiers]"control") -and ($key.key -eq "C"))
+            {
+                $answer = Read-Host "`nProcessing paused at point #$($pwds). Continue? [S/n]"
+                if ($answer.ToLower() -eq 'n') {
+                    Write-Host "Bye!"
+                    $continue = $false
+                    $reader.Dispose()
+                    break
+                } else {
+                    Write-Host "Resuming cracking process, point #$($pwds)."
+                    [console]::TreatControlCAsInput = $true
+                }
+            }
+        }
+        $pwds++
     }
-    # Intenta obrir el fitxer de PowerPoint 
+
+    # Tanca l'objecte PowerPoint
     try {
-        $Presentation = $PowerPoint.ProtectedViewWindows.Open($PowerPointFile, $Password)
-        $Presentation.Close()
         $PowerPoint.Quit()
-        $trencat = 1
-        $continue = $false
-        break
     }
     catch {
     }
+} 
+finally {
+    $pwds = $pwds - 1
+    Write-Host "`nSaving resume file to point $($pwds)..." -NoNewline
+    $pwds | Out-File -FilePath $ResumeFile
+    Write-Host "OK"
 
-    if ([console]::KeyAvailable)
-    {
-        $key = [system.console]::readkey($true)
-        if (($key.modifiers -band [consolemodifiers]"control") -and ($key.key -eq "C"))
-        {
-            $answer = Read-Host "`nProcessing paused at point #$($pwds). Continue? [S/n]"
-            if ($answer.ToLower() -eq 'n') {
-                Write-Host "Bye!"
-                $continue = $false
-                $reader.Dispose()
-                break
-            } else {
-                Write-Host "Resuming cracking process, point #$($pwds)."
-                [console]::TreatControlCAsInput = $true
-            }
-        }
+    if ($trencat) {
+        Write-Host "`n'$PowerPointFile' cracked with password '$Password'"
+    } else {
+        Write-Host "`nSorry, couldn't crack file '$PowerPointFile'"
     }
-    $pwds++
-}
-
-# Tanca l'objecte PowerPoint
-try {
-    $PowerPoint.Quit()
-}
-catch {
-}
-
-if ($trencat) {
-    Write-Host "`n'$PowerPointFile' cracked with password '$Password'"
-} else {
-    Write-Host "`nSorry, couldn't crack file '$PowerPointFile'"
 }
